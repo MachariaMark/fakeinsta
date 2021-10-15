a3d7bfd96bee89eee8765ea17993067bb27b40d7
1offmark <official1offmark@gmail.com>
2021-10-15 22:03:39 +0300

    added nessesary imports to  models.py

--- insta/models.py
+++ insta/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
-# Create your models here.
+from django.contrib.auth.models import User
+import datetime as dt
+from pyuploadcare.dj.models import ImageField
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
