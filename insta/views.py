17632e67ce5dea318bb6be5a6635ada9931b0aa7
1offmark <official1offmark@gmail.com>
2021-10-15 22:25:39 +0300

    added nessesary imports to  views.py

--- insta/views.py
+++ insta/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render,redirect,get_object_or_404
+from django.contrib.auth import login, views, forms
+from django.contrib.auth.decorators import login_required
+from .models import *
+from django.contrib.auth.models import User
+from .forms import LikesForm, CommentsForm, UpdateProfileForm, UploadPicForm
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import HttpResponse,Http404,HttpResponseRedirect
