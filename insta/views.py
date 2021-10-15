2e2b3bf9a2d27947af47d568fb83d4c4046d3c4e
1offmark <official1offmark@gmail.com>
2021-10-15 22:36:13 +0300

    added profile page to views.py

--- insta/views.py
+++ insta/views.py
@@ -80,3 +80,18 @@ def search_results(request):
     message = "You haven't searched for any term"
     return render(request, 'insta_pages/search.html',locals())
 
+#profile page
+@login_required(login_url='/accounts/login')
+def profilePage(request):
+  likesForm = LikesForm
+  commentForm = CommentsForm
+  images = Image.objects.all()
+  user = request.user.get_username()
+  current_user = request.user
+  photos = Image.objects.filter(profile=current_user.id)
+  profile = Profile.objects.all()
+  likes = Like.objects.all()
+ 
+    
+  return render(request,'insta_pages/profile.html', locals())
+  
