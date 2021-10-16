61cc019bb161097b51820e62f3a43d6f9821137e
1offmark <official1offmark@gmail.com>
2021-10-16 22:54:12 +0300

    pip installed dj-database-url

--- fakeinsta/settings.py
+++ fakeinsta/settings.py
@@ -84,10 +84,13 @@ WSGI_APPLICATION = 'fakeinsta.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+import dj_database_url
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'fakeinsta',
+        'USER': 'marknuchirimacharia',
+        'PASSWORD':'1offmark',
     }
 }
 
